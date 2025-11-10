6b9b147c72fd9ce6377035e8b19e13f7aab81250
Sammy Boy <samueltsutcliffe@gmail.com>
2025-11-10 18:10:52 +0000

    I have added the emoji svg's to the app and loaded them on the form

--- src/components/AddFeelingForm.jsx
+++ src/components/AddFeelingForm.jsx
@@ -1,3 +1,14 @@
+import Angry from '../assets/angry.svg'
+import Anxious from '../assets/anxious.svg'
+import Detatched from '../assets/detatched.svg'
+import Exhausted from '../assets/exhausted.svg'
+import Scared from '../assets/scared.svg'
+import Sad from '../assets/sad.svg'
+import Indifferent from '../assets/indifferent.svg'
+import Stressed from '../assets/stressed.svg'
+import Loved from '../assets/loved.svg'
+import Happy from '../assets/happy.svg'
+
 export default function AddFeelingForm (props)  {
     return (
         <div className="add-feeling-form__wrapper">
@@ -7,10 +18,20 @@ export default function AddFeelingForm (props)  {
                 <textarea name={"mood"} />
                 <p>Choose an emoji</p>
                 <div className="add-feeling-form__imoji-buttons">
-                    <img alt={"emoji"} className="add-feeling-form__imoji-button" src={""}/>
-                    <img alt={"emoji"} className="add-feeling-form__imoji-button" src={""}/>
-                    <img alt={"emoji"} className="add-feeling-form__imoji-button" src={""}/>
-                    <img alt={"emoji"} className="add-feeling-form__imoji-button" src={""}/>
+                    <div className="add-feeling-form__imoji-button__container">
+                        <img alt={"emoji"} className="add-feeling-form__imoji-button" src={Angry}/>
+                        <img alt={"emoji"} className="add-feeling-form__imoji-button" src={Anxious}/>
+                        <img alt={"emoji"} className="add-feeling-form__imoji-button" src={Detatched}/>
+                        <img alt={"emoji"} className="add-feeling-form__imoji-button" src={Exhausted}/>
+                        <img alt={"emoji"} className="add-feeling-form__imoji-button" src={Scared}/>
+                    </div>
+                    <div>
+                        <img alt={"emoji"} className="add-feeling-form__imoji-button" src={Sad}/>
+                        <img alt={"emoji"} className="add-feeling-form__imoji-button" src={Indifferent}/>
+                        <img alt={"emoji"} className="add-feeling-form__imoji-button" src={Stressed}/>
+                        <img alt={"emoji"} className="add-feeling-form__imoji-button" src={Loved}/>
+                        <img alt={"emoji"} className="add-feeling-form__imoji-button" src={Happy}/>
+                    </div>
                 </div>
             </form>
 
